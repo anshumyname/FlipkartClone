67c0f61b4b190d65ca9a1009715bfa1e0a404141
Anshuman Srivastava <srivastavaanshuman33@gmail.com>
2021-11-11 15:59:22 +0530

    made login dialogbox

--- server/index.js
+++ server/index.js
@@ -3,10 +3,19 @@ import mongoose from "mongoose";
 import { Connection } from "./database/db.js";
 import dotenv from 'dotenv';
 import defaultData from './default.js';
-
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import Routes from './routes/route.js'
 
 dotenv.config()
 const app = express();
+
+app.use(bodyParser.json({extended: true}))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(cors())
+app.use('/',Routes)
+
+
 
 const PORT = 8000;
 
